337b926465904667cdd2048ffe676431a8920753
selmanclskn <selman.clskn@hotmail.com>
2025-02-11 20:04:43 +0300

    fe_camp-commit_1

---
+++ javascript/fundamentals/part-I/script.js
@@ -0,0 +1,33 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Selman');
+console.log(29);
+
+let firstName = 'Selman';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let selman_dev = 'SÇ';
+let $function = 29;
+
+let person = 'Selman';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Freelancer';
+
+let job1 = 'Programmer';
+let job2 = 'Freelancer';
+
+console.log(myFirstJob);
+
+//  # Assigment: Values and Variables
+let country = 'Türkiye';
+let continent = 'Europe, Asia';
+let population = 81000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
