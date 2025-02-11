b9d2a8e6ad0c7cd45b59bf2e82c597ae8aafc6ca
selmanclskn <selman.clskn@hotmail.com>
2025-02-12 00:42:40 +0300

    fe_camp-commit_2

--- javascript/fundamentals/part-I/script.js
+++ javascript/fundamentals/part-I/script.js
@@ -1,3 +1,5 @@
+/** 
+
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -31,3 +33,63 @@ let population = 81000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Selman');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1996;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+//  # Assigment: Data Types
+let country = 'Türkiye';
+let continent = 'Europe, Asia';
+let population = 81000000;
+let isIsland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
+
+
+let age = 29;
+age = 31;
+
+const birthYear = 1996;
+// birthYear = 1999; const ile tanımlanan değerler değiştirilemez !!
+
+var job = 'designer';
+job = 'programmer';
+
+lastName = 'Çalışkan';
+console.log(lastName);
+
+
+//  # Assigment: let, const and var
+const country = 'Türkiye';
+const continent = 'Europe, Asia';
+let population = 81000000;
+const isIsland = false;
+const language = 'Türkçe';
+
+*/
