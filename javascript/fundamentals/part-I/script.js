1884cc9bfee62252c5099e45b4ab70f2d7eb7d19
selmanclskn <selman.clskn@hotmail.com>
2025-02-24 16:50:12 +0300

    fe_camp-commit_4

--- javascript/fundamentals/part-I/script.js
+++ javascript/fundamentals/part-I/script.js
@@ -92,4 +92,94 @@ let population = 81000000;
 const isIsland = false;
 const language = 'Türkçe';
 
+
+
+const now = 2025;
+const ageSelman = now - 1996;
+const ageJonas = now - 1991;
+console.log(ageSelman, ageJonas);
+
+console.log(
+  ageJonas * 2,
+  ageJonas / 2,
+  ageJonas + 2,
+  ageJonas - 2,
+  ageJonas ** 3 // ageJonas * ageJonas * ageJonas
+);
+
+const firstName = 'Selman';
+const lastName = 'Çalışkan';
+console.log(firstName + ' ' + lastName);
+
+// Assigment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1
+x--; // x = x -1
+console.log(x);
+
+// Comparison operators
+console.log(ageSelman > ageJonas); // <, >, <=, >=
+
+const isFullAge = ageJonas >= 18;
+
+console.log(now - 1996 > now - 1991);
+
+
+
+//  # Assigment: basic operators
+let population = 80000000;
+console.log(population / 2);
+population++;
+console.log(population);
+let populationTurkey = 80000000;
+let populationFinland = 6000000;
+console.log(populationTurkey > populationFinland);
+console.log(populationTurkey > 33000000);
+let country = 'Turkey';
+let language = 'Turkish';
+let continent = 'Europa';
+console.log(
+  country +
+    ' ' +
+    'is in' +
+    ' ' +
+    continent +
+    ', ' +
+    'and its ' +
+    population +
+    ' people speak ' +
+    language
+);
+
+
+
+const now = 2025;
+const ageSelman = now - 1996;
+const ageJonas = now - 1991;
+
+console.log(now - 1996 > now - 1991);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSelman) / 2;
+console.log(ageJonas, ageSelman, averageAge);
+
+
+
+// CHALLENGE #1
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / heightMark;
+let BMIJohn = massJohn / heightJohn;
+
+console.log(BMIMark, BMIJohn);
+console.log(BMIMark < BMIJohn);
 */
